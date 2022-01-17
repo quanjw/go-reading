ecd624d1981e8930e260b6eb5e9ae2700bdb5dc1
quanjw <516013775@qq.com>
2022-01-17 22:33:48 +0800

    gin generate jwt

--- conf/config.go
+++ conf/config.go
@@ -1,6 +1,8 @@
 package conf
 
 const (
-	Domain  = "localhost"
-	Baseurl = "http://localhost:8081"
+	Domain        = "localhost"
+	Baseurl       = "http://localhost:8081"
+	OneDayOfHours = 60 * 60 * 24
+	SECRET        = "hello gin"
 )
--- handler/user.go
+++ handler/user.go
@@ -1,12 +1,14 @@
 package handler
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"go-reading/conf"
 	"go-reading/model"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func Register(c *gin.Context) {
@@ -53,10 +55,32 @@ func Login(c *gin.Context) {
 		user := loginUser.QueryByEmail()
 		if loginUser.Password == user.Password {
 			c.SetCookie("gin_user", strconv.FormatInt(user.Id, 10), 7*24*3600, "/", conf.Domain, false, true)
-			c.JSON(200, gin.H{
-				"message": "用户" + user.Username + "已登陆",
-				"success": "true",
-			})
+
+			expiresTime := time.Now().Unix() + int64(conf.OneDayOfHours)
+			calims := jwt.StandardClaims{
+				Audience:  user.Username,     // 受众
+				ExpiresAt: expiresTime,       // 失效时间
+				Id:        string(user.Id),   // 编号
+				IssuedAt:  time.Now().Unix(), // 签发时间
+				Issuer:    "quanjw",          // 签发人
+				NotBefore: time.Now().Unix(), // 生效时间
+				Subject:   "login",           // 主题
+			}
+			tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, calims)
+			jwtSecret := []byte(conf.SECRET)
+			if token, err := tokenClaims.SignedString(jwtSecret); err == nil {
+				c.JSON(200, gin.H{
+					"message": "用户" + user.Username + "已登陆",
+					"success": "true",
+					"data":    token,
+				})
+			} else {
+				c.JSON(200, gin.H{
+					"message": "生成token错误",
+					"success": "true",
+				})
+			}
+
 		} else {
 			c.JSON(200, gin.H{
 				"message": "登陆失败，用户名或密码错误",
